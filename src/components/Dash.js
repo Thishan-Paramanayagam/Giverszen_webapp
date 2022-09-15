3b572bde6dfadeff2b616b7fe7885ef7e7f4fd39
Thishan-Paramanayagam <84135246+Thishan-Paramanayagam@users.noreply.github.com>
2022-09-15 06:41:23 +0530

    Dashboard

---
+++ src/components/Dash.js
@@ -0,0 +1,87 @@
+import React from 'react'
+
+function Dash() {
+  return (
+    <div name='dash'>
+        
+    <div class="p-5 ml-64">
+          
+             
+          
+      
+      
+      <div class="font-bold mb-2 text-center text-4xl mt-28 ">Overview</div>
+      <div class=" shadow-green-900">
+
+              <div class=" w-full lg:max-w-full lg:flex bg-white justify-center text-black">
+
+
+                  <div class="p-10">
+
+                      <div class="max-w-sm rounded overflow-hidden shadow-2xl" id="cd">
+
+                          <div class="px-10 py-4 bg-green-200 w-64 h-40">
+                              <div class="font-bold text-3xl mb-2 text-center">Total Users</div>
+                              <br/>
+                              <h1 class="text-center font-bold text-2xl">438</h1>
+                          </div>
+
+                      </div>
+                  </div>
+                  <div class="p-10">
+
+                      <div class="max-w-sm rounded overflow-hidden shadow-2xl" id="cd">
+
+                          <div class="px-10 py-4 bg-green-200 w-64 h-40" >
+                              <div class="font-bold text-3xl mb-2 text-center">Total Complaints</div>
+                              <h1 class="text-center font-bold text-2xl">29</h1>
+                          </div>
+
+                      </div>
+                  </div>
+                  <div class="p-10">
+
+                      <div class="max-w-sm rounded overflow-hidden shadow-2xl" id="cd">
+
+                          <div class="px-10 py-4 bg-green-200 w-64 h-40">
+                              <div class="font-bold text-3xl mb-2 text-center">Total Listings</div>
+                              <h1 class="text-center font-bold text-2xl">132</h1>
+                          </div>
+
+                      </div>
+                  </div>
+                 
+                  </div>
+              </div>
+              <div class=" w-full lg:max-w-full lg:flex bg-white justify-center text-black">
+              <div class="p-10">
+
+<div class="max-w-sm rounded overflow-hidden shadow-2xl" id="cd">
+
+    <div class="px-10 py-4 bg-green-200 w-64 h-40" >
+        <div class="font-bold text-3xl mb-2 text-center">Pending Listings</div>
+        <h1 class="text-center font-bold text-2xl">49</h1>
+    </div>
+
+</div>
+</div><div class="p-10">
+
+<div class="max-w-sm rounded overflow-hidden shadow-2xl" id="cd">
+
+    <div class="px-10 py-4 bg-green-200 w-64 h-40" >
+        <div class="font-bold text-3xl mb-2 text-center">Total Wanted</div>
+        <h1 class="text-center font-bold text-2xl">100</h1>
+    </div>
+
+</div>
+              </div>
+
+          </div>
+         
+          </div>
+          </div>
+
+  );
+}
+
+export default Dash
